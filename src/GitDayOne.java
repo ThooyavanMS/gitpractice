9d2688bd33234f30f5d2c5a0ce529e2e132d2ba9
ThooyavanMS <thooyavan081@gmail.com>
2023-07-20 11:32:11 +0530

    new one

--- src/GitDayOne.java
+++ src/GitDayOne.java
@@ -3,4 +3,8 @@ public class GitDayOne {
 public static void main(String[] args) {
 	System.out.println("Git Day One");
 }
+public void search() {
+System.out.println("this is new");
+System.out.println("try to merge");
+}
 }
