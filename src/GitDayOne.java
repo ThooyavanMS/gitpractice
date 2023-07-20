e97804fb795b8ed9b512bed6a08002debcea98f0
ThooyavanMS <thooyavan081@gmail.com>
2023-07-20 10:46:08 +0530

    Thooya

--- src/GitDayOne.java
+++ src/GitDayOne.java
@@ -4,4 +4,7 @@ public static void main(String[] args) {
 	System.out.println("Git Day One");
 	System.out.println("Check Branch One");
 }
+public void login() {
+	System.out.println("My Login Page");
+}
 }
